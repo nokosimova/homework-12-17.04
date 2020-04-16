b45a96e581bde1c5182ce3d5858edd50b3ab5d0a
unknown <nkosimova06@gmail.com>
2020-04-17 00:38:41 +0500

    creating class ArrayHepler

---
+++ ArrayHeplerClass.cs
@@ -0,0 +1,29 @@
+using System;
+namespace ArrayHelper
+{
+    static class ArrayHelper<T>
+    {
+        public static T Pop(ref T[] array)
+        {
+
+        }
+        public static int Push(ref T[] array, T new_element)
+        {
+
+        }
+        public static T Shift(ref T[] array)
+        {
+
+        }
+        public static int UnShift(ref T[] array, T new_element)
+        {
+
+        }
+        public static Slice(ref T[] array, int begin_index, int end_index)
+        {
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ArrayHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
