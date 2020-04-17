558ddebed09ba4505d0505f85f107478f59a1216
unknown <nkosimova06@gmail.com>
2020-04-18 00:09:56 +0500

    final code

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ namespace ArrayHelper
     {
         static void Main(string[] args)
         {
-            char[] array = new char[] {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j', 'k'};
+            char[] array = new char[] {'a', 'b', 'c', 'd', 'e', 'f'};
+            Console.WriteLine("Исходный массив");
+            for(int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+            Console.Write("\n");
             Console.WriteLine(@"Выберите команду:
-Pop------1
-Push-----2
-Shift----3
-Unshift--4
-Slice----5
-Exit-----6");
+Pop------1   Push-----2
+Shift----3   Unshift--4
+Slice----5   Exit-----6");
             int c = int.Parse(Console.ReadLine());
             while (c != 6)
             {
@@ -22,12 +25,11 @@ Exit-----6");
                     case 1:
                         Console.WriteLine($"Удаленный элемент: {ArrayHelper<char>.Pop(ref array)}");
                         Console.WriteLine("Изменённый массив:");
-
                         for(int i = 0; i < array.Length; i++)
                         {
                             Console.Write($"{array[i]} ");
                         }
-                    break;
+                        break;
                     case 2:
                         Console.Write("добавляемый элемент = ");
                         char adding_element = char.Parse(Console.ReadLine());
@@ -38,18 +40,64 @@ Exit-----6");
                         {
                             Console.Write($"{array[i]} ");
                         }
-                    break;
+                        break;
                     case 3:
-                    break;
+                        Console.WriteLine($"Удаленный элемент: {ArrayHelper<char>.Shift(ref array)}");
+                        Console.WriteLine("Изменённый массив:");
+                        for(int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
                     case 4:
-                    break;
+                        Console.Write("добавляемый элемент = ");
+                        adding_element = char.Parse(Console.ReadLine());
+                        
+                        ArrayHelper<char>.UnShift(ref array, adding_element);
+                        Console.WriteLine("Изменённый массив:");
+                        for(int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
                     case 5:
-                    break;
+                        int? begin = null, end = null;
+                        Console.WriteLine("начальный индекс: указать-0  | не указывать-1");
+                        int cc = int.Parse(Console.ReadLine());
+                        if (cc == 0)
+                        {
+                            Console.Write("begin_index = ");
+                            begin = int.Parse(Console.ReadLine());
+                        }
+                        Console.WriteLine("конечный индекс: указать-0  | не указывать-1");
+                        cc = int.Parse(Console.ReadLine());
+                        if (cc == 0)
+                        {
+                            Console.Write("end_index = ");
+                            end = int.Parse(Console.ReadLine());
+                        }
+                        char[] new_array = ArrayHelper<char>.Slice(ref array,begin,end);
+                        Console.WriteLine("Подмассив:");
+                        for(int i = 0; i < new_array.Length; i++)
+                        {
+                            Console.Write($"{new_array[i]} ");
+                        }
+                        break;
                     case 6:
                     break;
                     default:
                     break;
                 }
+                if (c != 6)
+                {
+                    Console.Write("\n");
+                    Console.WriteLine(@"Выберите команду:
+Pop------1   Push-----2
+Shift----3   Unshift--4
+Slice----5   Exit-----6");
+                    c = int.Parse(Console.ReadLine());
+                }
+                
             }
 
 
